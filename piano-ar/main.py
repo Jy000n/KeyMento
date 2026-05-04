7c87b30b68d6fc9e60adfdd7fdaacfd151f17e99
jy000n <jayoonchang@naver.com>
2026-05-04 15:59:35 +0900

    chore: remove unnecessary comments

--- piano-ar/main.py
+++ piano-ar/main.py
@@ -20,16 +20,14 @@ while True:
     h, w = warped.shape[:2]
     whites, blacks = build_keys(w, h)
 
-    # 흰 건반 테두리
     for key in whites:
         x1, y1, x2, y2 = key
         cv2.rectangle(warped, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-    # 검은 건반: 채우기 + 테두리
     for key in blacks:
         x1, y1, x2, y2 = key
-        cv2.rectangle(warped, (x1, y1), (x2, y2), (50, 50, 50), -1)   # 채우기
-        cv2.rectangle(warped, (x1, y1), (x2, y2), (0, 200, 255), 1)   # 테두리
+        cv2.rectangle(warped, (x1, y1), (x2, y2), (50, 50, 50), -1)  
+        cv2.rectangle(warped, (x1, y1), (x2, y2), (0, 200, 255), 1)  
 
     cv2.imshow("Warped", warped)
 
