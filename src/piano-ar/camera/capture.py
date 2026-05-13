16be6242a5cfecddae92f92c9193b1348de6fa6c
turn-world <whdydwns0604@gmail.com>
2026-05-13 12:59:07 +0900

    feat : camera capture resoultion

--- src/piano-ar/camera/capture.py
+++ src/piano-ar/camera/capture.py
@@ -2,8 +2,11 @@ import cv2
 
 def open_camera(index=0):
     cap = cv2.VideoCapture(index)
-    
+
     if not cap.isOpened():
         raise Exception("카메라 연결 실패")
-    
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
     return cap
